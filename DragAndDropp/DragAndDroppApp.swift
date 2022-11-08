47c346a79558d34ca3ab0c007c0543f1619c1248
Javad <sir.hxt@gmail.com>
2022-11-08 10:28:18 +0330

    Initial Commit

---
+++ DragAndDropp/DragAndDroppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragAndDroppApp.swift
+//  DragAndDropp
+//
+//  Created by enigma 1 on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct DragAndDroppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
